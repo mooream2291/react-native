bc491c8a8a281aad3fb16a93b7e905bd592ff399
Ashley Moore <mooream2291@gmail.com>
2021-02-03 20:59:38 -0500

    find-me

---
+++ src/screens/FindMe.js
@@ -0,0 +1,59 @@
+import React, { component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { constants, Location, Permissions } from 'expo';
+
+class FindMe extends component {
+    state = {
+        latitude: null,
+        longitude: null,
+        errorMessage: null
+    };
+
+    findCurrentLocation = 
+    () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = JSON.stringify(position.coords.latitude);
+                const longitude = JSON.stringify(position.coords.longitude);
+
+                this.setState({
+                    latitude,
+                    longitude
+                });
+            },
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+    };
+
+    findCurrentLocationPermissions = async () => {
+        const { status } = await Permissions.askAsync
+        (Permissions.LOCATION);
+
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Access Denied'
+            });
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        this.setState({ location });
+    }
+    render() {
+        let text = '';
+        if (this.state.errorMessage) {
+            text = this.state.errorMessage;
+        } else if (this.state.location) {
+            text = JSON.stringify(this.state.location);
+        }
+        return (
+            <View>
+                <TouchableOpacity onPress={this.findCurrentLocation}>
+                    <Text> I am Here. </Text>
+                    <Text>{text}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default FindMe;
